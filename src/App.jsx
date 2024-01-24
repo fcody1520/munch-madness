6d4a49dd9b9a2167c82393ce4f1746507b9a8bfb
Cody Farrington <fcody1520@gmail.com>
2024-01-24 11:34:03 -0700

    add state variables to login, started functionality of handlelogin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Login from './components/Login'
 import Edit from './components/Edit'
+import Bracket from './components/Bracket'
 
 function App() {
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,30 @@
 import React, {useState} from 'react'
 // import {biUserCircle} from 'react-icons/bi'
+import axios from 'axios'
 
 export default function Login() {
     
     const [action, setAction] = useState('Sign Up') 
-  
+    const [emailInput, setEmailInput] = useState('')
+    const [passwordInput, setPasswordInput] = useState('')
+    const [firstNameInput, setFirstNameInput] = useState('')
+    const [lastNameInput, setLastNameInput] = useState('')
 
+    async function handleLogIn(){
+        let signMaBod = {
+             email: emailInput,
+             password: passwordInput
+        }
 
-    function handleLogIn(){
+        try{ 
+        const response = await axios.post('/login', signMaBod)
+        
+        } catch (error) {
+            console.error('Error during post request:', error)
+        }
+    };
+
+    function handleSignUp(){
         
     }
   
@@ -21,18 +38,42 @@ export default function Login() {
         <div className='inputs'>
             {action==="Login" ? <div></div>: <div className='input'>
                 {/* <img src={biUserCircle} alt="" /> */}
-                <input id='First Name' type="text" placeholder='First Name' required />
-                <input id='Last Name' type="text" placeholder='Last Name' required />
+                <input 
+                id='first-name' 
+                value={firstNameInput}
+                type="text" 
+                placeholder='First Name'
+                onChange={(e) => setFirstNameInput(e.target.value)} 
+                required />
+                <input 
+                id='last-name'
+                value={lastNameInput} 
+                type="text" 
+                placeholder='Last Name'
+                onChange={(e) => setLastNameInput(e.target.value)} 
+                required />
             </div>
             }
             
             <div className='input'>
                 <img src="" alt="" />
-                <input id='Email' type="email" placeholder='Email' required/>
+                <input 
+                id='Email' 
+                value={emailInput}
+                type="email" 
+                placeholder='Email'
+                onChange={(e) => setEmailInput(e.target.value)} 
+                required/>
             </div>
             <div className='input'>
                 <img src="" alt="" />
-                <input id='Password' type="password" placeholder='Password' required/>
+                <input 
+                id='Password' 
+                value={passwordInput}
+                type="password" 
+                placeholder='Password'
+                onChange={(e) => setPasswordInput(e.target.value)} 
+                required/>
             </div>
         </div>
         {/* <div className="forgot-password">Lost Password? <span>Click Here!</span></div> turn this into ternary also */}
@@ -40,8 +81,10 @@ export default function Login() {
             <div className={action==='Login'?'submit gray': "submit"}onClick={() => {setAction('Sign Up')}}>Sign Up</div>
             <div className={action==='Sign Up'?'submit gray': "submit"}
             onClick={() => {setAction('Login')}}>Login</div>
-        </div>
-        <button onClick={handleLogIn}>Let's Munch!</button>
+        </div>{action === 'Sign Up'?
+        <button onClick={handleSignUp}>Let Me Munch!</button>
+        :
+        <button onClick={handleLogIn}>Let's Munch!</button>}
     </>
   )
 }
