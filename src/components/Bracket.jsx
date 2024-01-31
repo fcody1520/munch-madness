1156debca8aa9729f27049566f9cdfa20cb6bf3b
Cody Farrington <fcody1520@gmail.com>
2024-01-31 11:52:31 -0700

    make Bracket.css

--- src/components/Bracket.jsx
+++ src/components/Bracket.jsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react'
 import Rounds from './Rounds'
 import Cards from './Cards'
 import axios from 'axios'
+import '../CSS/Bracket.css'
 
 
 export default function Bracket() {
