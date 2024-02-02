0a289dd2d52fd91601d6db814ca08abd7d54e436
Cody Farrington <fcody1520@gmail.com>
2024-02-02 09:49:50 -0700

    add shadowing to login and edit divs

--- src/components/Bracket.jsx
+++ src/components/Bracket.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Cards from "./Cards";
 import "../CSS/Bracket.css";
 import axios from "axios";
 import useWindowDimensions from "../customHooks/useWindowDimensions.jsx";
@@ -20,16 +19,6 @@ export default function Bracket({
       : null
   );
 
-  // const [r1Winners, setR1Winners] = useState([]);
-  // const [r2Winners, setR2Winners] = useState([]);
-  // const [winner, setWinner] = useState([]);
-  // const [rests, setRests] = useState([]);
-  // const [round, setRound] = useState(1);
-
-  // make a useEffect, inside watch r1 winners and r2 winners
-  // if there's a change, fire a function that checks the length of the sets(winners)
-  // if r1.length is 4, we need to update the round by one
-  //  if r2 length is 2, update round to be round 3
 
   useEffect(() => {
     if (!manualContenders) {
@@ -42,7 +31,6 @@ export default function Bracket({
         axios
           .get(`/restaurants/${latitude}/${longitude}`)
           .then((res) => {
-            // setRests(res.data.restInfo);
             let quantity = contenderQuantity;
             if (!quantity) {
               quantity = DEFAULT_QUANTITY;
@@ -90,39 +78,11 @@ export default function Bracket({
 
 
 
-  // const eachRest = rests.map((rest) => {
-  //   return (
-  //     <Cards
-  //       rest={rest}
-  //       rests={rests}
-  //       setR1Winners={setR1Winners}
-  //       setR2Winners={setR2Winners}
-  //       setWinner={setWinner}
-  //       round={round}
-  //       setRound={setRound}
-  //     />
-  //   )
-  // });
-
-      // const address = node?.value?.address.join(' ')
-
-
-  // console.log(r1Winners);
+  
   return (
     <>
       <h1>Munch Madness!</h1>
-      {/* <br />
-      {eachRest[0]}
-      {eachRest[1]}
-      <br />
-      {eachRest[2]}
-      {eachRest[3]}
-      <br />
-      {eachRest[4]}
-      {eachRest[5]}
-      <br />
-      {eachRest[6]}
-      {eachRest[7]} */}
+      
       {
         tree
           ?<div
@@ -139,7 +99,6 @@ export default function Bracket({
                           top: `${y}px`,
                           left: 0,
                           display: 'flex',
-                          // display: 'grid',
                           gridTemplateColumns: ``,
                           flexDirection: 'row',
                           justifyContent: 'space-around',
@@ -195,7 +154,6 @@ function createTreeArrs(tree) {
     }
   }
 
-  // console.log(returnArr);
   return returnArr;
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,42 @@
-import React from 'react'
-import { NavLink,useNavigate } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserGear } from '@fortawesome/free-solid-svg-icons'
-import axios from 'axios'
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserGear,faGears,faToolbox } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
+export default function Navbar({ userData, setLoggedIn }) {
+  const navigate = useNavigate();
 
-export default function Navbar({userData, setLoggedIn}) {
-  const navigate = useNavigate()
-  
-    async function handleLogout(){
-        try{    
-        await axios.delete('/logout')
-        setLoggedIn(false)
-        navigate('/')
-        } catch (error){
-            console.error('Error during logout request:', error)
-        }
+  async function handleLogout() {
+    try {
+      await axios.delete("/logout");
+      setLoggedIn(false);
+      navigate("/");
+    } catch (error) {
+      console.error("Error during logout request:", error);
     }
+  }
 
-    return (
+  return (
     <>
-        <div className="navbar">
-          <span className='navbar__welcome'>Welcome, {userData.fname}. Let's Munch!</span>
-          <div className='navbar__image'>
-            <img src="./public/munch_madness_logo.png" alt="" />
-          </div>
-            <div className='navbar__button'>
-              <NavLink to='/edit-info' className='navbar'><FontAwesomeIcon icon={faUserGear}/></NavLink>
-              <button className='navbar' onClick={handleLogout}>Logout</button>
-            </div>
+      <div className="navbar">
+        <span className="navbar__welcome">
+          Welcome, {userData.fname}. Let's Munch!
+        </span>
+        <div className="navbar__image">
+          <img src="./public/munch_madness_logo.png" alt="" />
         </div>
+        <div className="navbar__button">
+          <div>
+            <NavLink to="/edit-info" className="navbar">
+              <FontAwesomeIcon icon={faToolbox} />
+            </NavLink>
+            </div>
+          <button className="navbar" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
